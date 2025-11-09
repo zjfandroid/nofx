c95fd7e851595b0796ad22d7835f31c280916e03
J.Feng <e2a@foxmail.com>
2025-11-09 14:08:35 +0800

    ip

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        // target: 'http://localhost:8080',
+        target: 'http://52.197.196.213:8080',
         changeOrigin: true,
       },
     },
